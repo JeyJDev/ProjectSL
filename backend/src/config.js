0df48a3d729d23d7967928c36e87ade47fca180e
JeyJDev <josejorgefisi1620@gmail.com>
2024-03-27 23:07:06 +0000

    variable de entorno corregida

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,6 +1,6 @@
 export const TOKEN_SECRET = "secretKey"
 
-export const FRONTEND_URL = process.env.DB_HOST || "http://localhost:5173"
+export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173"
 
 export const DB_HOST = process.env.DB_HOST || "mongodb+srv://jose:S28jyUJt0NqgO3CS@cluster0.5i1p3ph.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
