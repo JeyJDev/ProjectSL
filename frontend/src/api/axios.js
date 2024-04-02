393efb3f314c94e325ca39a8093f335f686e1852
JeyJDev <165077983+JeyJDev@users.noreply.github.com>
2024-04-02 17:32:04 -0500

    Update axios.js
    Se cambió la configuración de la variable de entono, de import.meta.env a process.env

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000"
+const URL = process.env.VITE_BACKEND_URL || "http://localhost:3000"
 
 const instance = axios.create({
     baseURL: `${URL}/api`,
