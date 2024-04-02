b745b053a207abfa7c815f405b34daf92e195f85
JeyJDev <165077983+JeyJDev@users.noreply.github.com>
2024-04-02 18:06:40 -0500

    Update config.js
    Se cambión la configuración de la conexión a la base de datos

--- backend/src/config.js
+++ backend/src/config.js
@@ -2,6 +2,6 @@ export const TOKEN_SECRET = "secretKey"
 
 export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173"
 
-export const DB_HOST = process.env.DB_HOST || "mongodb+srv://jose:S28jyUJt0NqgO3CS@cluster0.5i1p3ph.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+export const DB_HOST = process.env.DB_HOST
 
 export const PORT = process.env.PORT || 3000
