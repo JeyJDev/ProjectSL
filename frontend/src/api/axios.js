679bd3068bc40b034722aa8e949e44c97733af4e
JeyJDev <165077983+JeyJDev@users.noreply.github.com>
2024-04-02 17:52:29 -0500

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = process.env.VITE_BACKEND_URL || "http://localhost:3000"
+const URL = process.env.VITE_BACKEND_URL
 
 const instance = axios.create({
     baseURL: `${URL}/api`,
