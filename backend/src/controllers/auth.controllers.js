9ee10cdec9d7e71ae693621846ce3c743c93e3c5
JeyJDev <165077983+JeyJDev@users.noreply.github.com>
2024-04-03 17:14:20 -0500

    Update auth.controllers.js

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -120,7 +120,7 @@ export const login = async(req, res) => {
                 const token = await createAccessToken({id: playerFound._id})
                 isPlayer = true
                 res.cookie('token', token)
-                return res.json({
+                res.json({
                         isPlayer: isPlayer
                 })
             }
