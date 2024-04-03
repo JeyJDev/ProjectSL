c932ccc3799b126101a47759915e2e6af34613e6
JeyJDev <165077983+JeyJDev@users.noreply.github.com>
2024-04-03 15:26:23 -0500

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const URL = import.meta.env.VITE_BACKEND_URL
 
 const instance = axios.create({
-    baseURL: `${URL}/api`,
+    baseURL: `https://backend-api-pfp9.onrender.com/api`,
     withCredentials: true
 })
 
