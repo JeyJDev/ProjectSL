6feb1aed58ef58313fd442bdf6b602e1d5e0c6fc
JeyJDev <165077983+JeyJDev@users.noreply.github.com>
2024-04-03 13:11:28 -0500

    Update config.js

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,7 +1,7 @@
 export const TOKEN_SECRET = "secretKey"
 
-export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173"
+export const FRONTEND_URL = process.env.FRONTEND_URL
 
 export const DB_HOST = process.env.DB_HOST
 
-export const PORT = process.env.PORT || 3000
+export const PORT = process.env.PORT
